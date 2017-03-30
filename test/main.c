25e0132791d3ea09f80db29fd9db184f84396a01
gchh <skring.2008@163.com>
2017-03-30 16:56:22 +0800

    17-3-3-17h

--- test/main.c
+++ test/main.c
@@ -1,9 +1,38 @@
+#if 0
 #include <stdio.h>
 #include "acllib.h"
 
 ACL_Image img;
 
 ACL_Sound sound;
+
+int bt_old,event_old;
+
+void mouseEvent(int x,int y,int bt,int event)
+{
+	//printf("%d,%d ",bt_old,event_old);
+	if(bt==LEFT_BUTTON||bt==RIGHT_BUTTON)
+	{
+		bt_old=bt;
+		event_old=event;
+	}
+	if(event==BUTTON_DOWN)
+	{
+		beginPaint();
+		setBrushStyle(BRUSH_STYLE_SOLID);
+	    if(bt==LEFT_BUTTON)setBrushColor(RED);
+	    else if(bt==RIGHT_BUTTON)setBrushColor(BLUE);
+	    rectangle(x-10,y-10,x+10,y+10);
+		endPaint();
+	}
+    else if(event==MOUSEMOVE&&(bt_old==LEFT_BUTTON||bt_old==RIGHT_BUTTON)&&event_old==BUTTON_DOWN)
+	{
+		beginPaint();
+	    rectangle(x-10,y-10,x+10,y+10);
+		endPaint();	
+	} 	 
+}
+
 
 int Setup()
 {
@@ -16,6 +45,8 @@ int Setup()
 	scanf("%d",&height); 
 	//生成一个窗口 
 	initWindow("test",DEFAULT,DEFAULT,width,height);
+	//鼠标 
+	registerMouseEvent(mouseEvent);
 	beginPaint();
 	//线 
 	setPenColor(GREEN); //线颜色 
@@ -73,10 +104,60 @@ int Setup()
 	putImageScale(&img,0,400,100,100);
 	//putImageTransparent(&img,0,400,100,100,RED);
 	//声音
-	//声音
 	loadSound("D:/Users/atdo/Desktop/C_advance/test/drip_drop.wav",&sound);
 	playSound(sound,1);	
 	//stopSound(sound);
 	endPaint();
 	return 0;
 }
+#else
+
+#include <stdio.h>
+#include "acllib.h"
+int bt_old,event_old;
+void mouseEvent(int x,int y,int bt,int event)
+{
+	//printf("%d,%d ",bt_old,event_old);
+ 	if(bt==LEFT_BUTTON||bt==RIGHT_BUTTON)
+ 	{
+  		bt_old=bt;
+  		event_old=event;
+ 	}
+ 	if(event==BUTTON_DOWN)
+ 	{
+  		beginPaint();
+  		setBrushStyle(BRUSH_STYLE_SOLID);
+     	if(bt==LEFT_BUTTON)
+		{
+			setPenColor(RED); 
+			setBrushColor(RED);
+		}
+     	else if(bt==RIGHT_BUTTON)
+		{
+			setPenColor(BLUE);
+			setBrushColor(BLUE);
+		}
+     	rectangle(x-10,y-10,x+10,y+10);
+  		endPaint();
+ 	}
+    else if(event==MOUSEMOVE&&(bt_old==LEFT_BUTTON||bt_old==RIGHT_BUTTON)&&event_old==BUTTON_DOWN)
+ 	{
+  		beginPaint();
+     	rectangle(x-10,y-10,x+10,y+10);
+  		endPaint(); 
+ 	}   
+}
+int Setup()
+{
+	initConsole();
+	printf("输入宽度：\n");
+	int width;
+	scanf("%d",&width);
+	printf("输入高度：\n"); 
+	int height;
+	scanf("%d",&height); 
+	initWindow("test",DEFAULT,DEFAULT,width,height);
+	registerMouseEvent(mouseEvent);
+	return 0;
+}
+#endif
