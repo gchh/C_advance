f32ba5e1d083f3729d6129004cd73887070534d5
gchh <skring.2008@163.com>
2017-03-30 23:57:21 +0800

    17-3-30-23h

--- test/main.c
+++ test/main.c
@@ -114,8 +114,58 @@ int Setup()
 
 #include <stdio.h>
 #include "acllib.h"
+
+ACL_Sound sound;
+
+#define MLEN 64
+int len = 0,i=0;
+char str[MLEN];
+void charEvent(char c)
+{
+    if(len+1>=MLEN)
+    {
+        memset(str,0,sizeof(str));
+        len = 0;
+    }
+    str[len] = c;
+    ++len;
+    
+    beginPaint();
+    setTextSize(16);
+    paintText(0,50,str);
+    endPaint();
+}
+
+//int kx=200,ky=200;
+int kx,ky;
+void keyEvent(int key,int event)
+{
+    if(event!=KEY_DOWN)
+        return;
+    switch(key)
+    {
+    case VK_UP:
+        ky-=10;
+        break;
+    case VK_DOWN:
+        ky+=10;
+        break;
+    case VK_LEFT:
+        kx-=10;
+        break;
+    case VK_RIGHT:
+        kx+=10;
+        break;
+    }	
+    beginPaint();
+    //clearDevice();
+    setBrushColor(BLACK);
+    rectangle(kx-10,ky-10,kx+10,ky+10);
+    endPaint();	
+}
+
 int bt_old,event_old;
-void mouseEvent(int x,int y,int bt,int event)
+void mouseEvent(int mx,int my,int bt,int event)
 {
 	//printf("%d,%d ",bt_old,event_old);
  	if(bt==LEFT_BUTTON||bt==RIGHT_BUTTON)
@@ -127,37 +177,50 @@ void mouseEvent(int x,int y,int bt,int event)
  	{
   		beginPaint();
   		setBrushStyle(BRUSH_STYLE_SOLID);
+  		setPenColor(EMPTY); 
      	if(bt==LEFT_BUTTON)
 		{
-			setPenColor(RED); 
+			//setPenColor(RED); 
 			setBrushColor(RED);
 		}
      	else if(bt==RIGHT_BUTTON)
 		{
-			setPenColor(BLUE);
+			//setPenColor(BLUE);
 			setBrushColor(BLUE);
 		}
-     	rectangle(x-10,y-10,x+10,y+10);
+     	rectangle(mx-10,my-10,mx+10,my+10);
   		endPaint();
  	}
     else if(event==MOUSEMOVE&&(bt_old==LEFT_BUTTON||bt_old==RIGHT_BUTTON)&&event_old==BUTTON_DOWN)
  	{
   		beginPaint();
-     	rectangle(x-10,y-10,x+10,y+10);
+     	rectangle(mx-10,my-10,mx+10,my+10);
   		endPaint(); 
  	}   
 }
 int Setup()
 {
+	/*
 	initConsole();
 	printf("输入宽度：\n");
 	int width;
 	scanf("%d",&width);
 	printf("输入高度：\n"); 
 	int height;
-	scanf("%d",&height); 
+	scanf("%d",&height); */
+	int width=700;
+	int height=700;
 	initWindow("test",DEFAULT,DEFAULT,width,height);
 	registerMouseEvent(mouseEvent);
+	kx=getX()+20;
+	ky=getY()+20;
+	registerKeyboardEvent(keyEvent);
+	registerCharEvent(charEvent);
+	beginPaint();
+	rectangle(kx-10,ky-10,kx+10,ky+10);
+	loadSound("D:/Users/atdo/Desktop/C_advance/test/白噪音.mp3",&sound);
+	playSound(sound,1);	
+	endPaint();
 	return 0;
 }
 #endif
