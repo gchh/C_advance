15b8e8eaf307da59772ac848e7b3dd1bc5c5d4cf
gchh <skring.2008@163.com>
2017-04-08 17:04:16 +0800

    17-4-8-17h

--- my_strcat.c
+++ my_strcat.c
@@ -1,16 +1,25 @@
 #include <stdio.h> 
 
-char *my_strcat(char *dest,const char *src)
+//char *my_strcat(char *dest,const char *src)
+void my_strcat(char *dest,const char *src)
 {
 	unsigned int a=0;
 	char *result;
+#if 1 
 	result=dest;
 	
 	do{
 		result++;
 		a++;
 	}while(*result!='\0');
-	
+#else  //不改变dest的内容 
+	do{
+		*result=*dest;
+		result++;
+		dest++;
+		a++;
+	}while(*dest!='\0');
+#endif	
 	do{
 		*result=*src;
 		result++;
@@ -25,14 +34,16 @@ char *my_strcat(char *dest,const char *src)
 		result--;
 		a--;
 	};
-	return result;
+	//return result;
 }
 
 int main()
 {
-	char a[20],b[20];
+	char a[20],b[20],*p;
 	scanf("%s %s",a,b);
 	my_strcat (a, b);
+	//p=my_strcat (a, b);
 	printf("%s\n",a);
+	//printf("%s\n",p);
 	return 0;
 }
